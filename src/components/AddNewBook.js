b6be37374de7884e8505e73ef1b9a2b7ba205ae2
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2021-09-29 05:08:43 +0100

    Addnew book form

--- src/components/AddNewBook.js
+++ src/components/AddNewBook.js
@@ -1,12 +1,39 @@
 import React from 'react';
 
+const options = [
+  {
+    key: 'apple',
+    label: 'Apple',
+    value: 'apple',
+  },
+  {
+    key: 'mango',
+    label: 'Mango',
+    value: 'mango',
+  },
+  {
+    key: 'banana',
+    label: 'Banana',
+    value: 'banana',
+  },
+  {
+    key: 'pineapple',
+    label: 'Pineapple',
+    value: 'pineapple',
+  },
+];
+
 const AddBook = () => (
   <>
     <div className="add-book-container">
       <h4>ADD NEW BOOK</h4>
       <form>
-        <input type="text" placeholder="Title" />
-        <select name="categories" id="categories"/>
+        <input type="text" placeholder="Book Title" />
+        <select>
+          {options.map((option) => (
+            <option key={option.key} value={option.value}>{option.label}</option>
+          ))}
+        </select>
         <button type="submit">ADD BOOK</button>
       </form>
     </div>
