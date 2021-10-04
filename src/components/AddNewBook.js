12253c0143ddcd863dc765bd9268d8e8cae3db39
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2021-10-04 06:37:43 +0100

    update form submission logic

--- src/components/AddNewBook.js
+++ src/components/AddNewBook.js
@@ -11,30 +11,32 @@ const AddBook = () => {
   const dispatch = useDispatch();
 
   const [t, setTitle] = useState('');
-  const [c, setCategory] = useState('');
+  const [c, setCategory] = useState('Category');
   const addBooksAction = bindActionCreators(addBooksData, dispatch);
 
   const submitBookToStore = (e) => {
     e.preventDefault();
-    const newBook = {
-      item_id: uuidv4(),
-      title: t,
-      category: c,
-    };
+    if (t !== '' && c !== 'Category') {
+      const newBook = {
+        item_id: uuidv4(),
+        title: t,
+        category: c,
+      };
 
-    // dispatch an action and pass it the newBook object (your action's payload)
-    addBooksAction(newBook);
-    setTitle('');
-    setCategory('');
+      // dispatch an action and pass it the newBook object (your action's payload)
+      addBooksAction(newBook);
+      setTitle('');
+      setCategory('Category');
+    }
   };
 
   return (
     <div className="add-book-container">
       <h4>ADD NEW BOOK</h4>
       <form onSubmit={submitBookToStore}>
-        <input type="text" name="title" placeholder="Book Title" onChange={(e) => setTitle(e.target.value)} />
-        <select name="category" id="category" defaultValue="Category" onChange={(e) => setCategory(e.target.value)}>
-          <option disabled>Category</option>
+        <input type="text" name="title" placeholder="Book Title" value={t} onChange={(e) => setTitle(e.target.value)} required />
+        <select name="category" id="category" value={c} onChange={(e) => setCategory(e.target.value)} required>
+          <option value="Category" disabled>Category</option>
           <option value="Thriller">Thriller</option>
           <option value="Romance">Romance</option>
           <option value="Action">Action</option>
